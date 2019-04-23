57891e48121195891c39e771688c0c8e6d0d408f
cfzhang <cfzhang@forwardx.com>
2019-04-23 21:10:37 +0800

    update

---
+++ algorithm/greedy/dijkstra/dijkstra.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef struct {
+    int x;
+    int y;
+    
+}Node;
+
+class Dijkstra
+{
+    int findShortestPath(const Node &s, const Node &e, vector<Node> &path);
+};
+
+
+int main(int argc, char *argv[])
+{
+
+
+
+
+  return 0;
+}
