d72cb3eabc319043446d943fdfbecb5829611df7
cfzhang <13180368090@163.com>
2019-04-26 01:57:24 +0800

    update heapify

--- algorithm/divide_and_conquer/heap/Heap.cpp
+++ algorithm/divide_and_conquer/heap/Heap.cpp
@@ -1,4 +1,80 @@
 //
 // Created by MS on 2019/4/24.
 //
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <cmath>
 
+using namespace std;
+
+void swap(int &a, int &b) {
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
+
+class Heap {
+public:
+    Heap(const vector<int> &array) {
+        if (array.size() > 0) {
+            data_.insert(data_.begin(), -1);
+            data_.insert(data_.end(), array.begin(), array.end());
+            heapify();
+        }
+    }
+
+    //根据堆的性质, 只要保证部分有序即可, 即根节点大于左右节点的值.
+    // 将数组抽象为一个完全二叉树, 所以只要从最后一个非叶子节点向前遍历每一个节点即可.
+    // 如果当前节点比左右子树节点都大, 则已经是一个最大堆
+    // , 否则将当前节点与左右节点较大的一个交换,
+    // 并且交换过之后依然要递归的查看子节点是否满足堆的性质, 不满足再往下调整. 如此即可完成数组的堆化.
+    void heapify() {
+        int node_to_be_disposal = floor((data_.size() - 1) / 2);
+        while (node_to_be_disposal > 0) {
+            adjustHeap(node_to_be_disposal);
+            node_to_be_disposal--;
+        }
+    }
+
+    void adjustHeap(int node_index) {
+        if (node_index > (data_.size() - 1) / 2) {
+            return;
+        }
+        int next_disposal = adjust(node_index);
+        if(next_disposal != -1){
+            adjustHeap(next_disposal);
+        }
+    }
+
+    int adjust(int node_index){
+        int max_index = -1;
+        int left_child_index = node_index * 2;
+        int right_child_index = node_index * 2 + 1;
+        max_index = data_[left_child_index] > data_[right_child_index] ? left_child_index : right_child_index;
+        if(data_[max_index] > data_[node_index]){
+            swap(data_[max_index],data_[node_index]);
+        }
+        return max_index;
+    }
+
+    friend ostream &operator<<(ostream &os, const Heap &heap) {
+        stringstream ss;
+        ss << "data_: [";
+        for (int x = 0; x < heap.data_.size(); x++) {
+            ss << heap.data_[x] << ", ";
+        }
+        ss << "]" << endl;
+        os << ss.str();
+        return os;
+    }
+
+private:
+    vector<int> data_;
+};
+
+int main(int argc, char *argv[]) {
+    vector<int> array{3, 6, 2, 4, 1, 7, 8};
+    Heap heap(array);
+    cout << heap;
+}
---
+++ algorithm/divide_and_conquer/heap/README.md
@@ -0,0 +1,39 @@
+### Array Representation of Binary Tree
+* if a node is at index --- i
+* its left child is at  --- 2*i
+* its right child is at --- 2*i+1
+* its parent is at      --- ⌊i/2⌋ 
+* 当缺少某些节点时，需要留白处理机， i从1开始计算
+### Complete Binary Tree
+* Full Binary: 树的高度容许范围内不可再放下任何一个节点（最多2^(n+1)-1个节点），如果增加一个节点，则会导致树长高一层
+* Complete Binary： 在一维数组中表述时，节点与节点之间不可再有留白或者gaps； 高度为log n
+* Full --> Complete; Complete may not Full;
+#### Heap
+* is Complete Binary Tree
+* MaxHeap: parent > descendants
+* MinHeap: parent < descendants
+#### Insert & Delete
+Insert
+* insert to end free space
+* adjust child element with parent elements by value
+* O(1) --> O(log n)
+
+Delete
+* Heap只能删除根节点
+* 删除Root
+* last element -> root element
+* then adjust from top to down; compare 2 child and select the bigger
+
+Note: 删除一个节点放到最后留白的位置，迭代删完所有的节点后，会得到一个有序序列，也就是Heap-Sort
+#### Heap Sort
+* 创建一个Heap 通过一个接一个的插入来实现 O(log n)
+* delete 一个接一个的来得到有序序列 O(log n)
+* 2*log n---->O(log n)
+
+#### Heapify
+* 根据堆的性质, 只要保证部分有序即可, 即根节点大于左右节点的值. 将数组抽象为一个完全二叉树, 所以只要从最后一个非叶子节点向前遍历每一个节点即可. 如果当前节点比左右子树节点都大, 则已经是一个最大堆, 否则将当前节点与左右节点较大的一个交换, 并且交换过之后依然要递归的查看子节点是否满足堆的性质, 不满足再往下调整. 如此即可完成数组的堆化.
+
+
+#### Priority Queue
+* 如果是数字越小优先级越高，则建立一个MinHeap
+* 如果数字越大优先级越高，则建立一个MaxHeap
